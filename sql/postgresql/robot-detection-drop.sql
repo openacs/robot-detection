5c97851338ccd16f47dcfd801c46205981493c47
Domingo Alvarez duarte <domingo@dad-it.com>
2001-05-02 20:21:04 +0000

    Start postgresql port

---
+++ sql/postgresql/robot-detection-drop.sql
@@ -0,0 +1,7 @@
+-- packages/robot-detection/sql/robot-detection-drop.sql
+-- @author Roger Hsueh (rogerh@arsdigita.com)
+-- @creation-date  2000-12-01
+-- @cvs-id $Id$
+
+-- Drops robot-detection related table
+drop table robots;
